de1c8bd10b4e50a83a7360a693b54af66834a8d8
Prus.Oleksandr <125257602+Prus-Oleksandr@users.noreply.github.com>
2023-04-25 09:06:54 +0300

    Варіант 11
    Відсортувати студентів за середнім балом та вивести на екран
    тих, хто має бал більш за 4.

--- main.cpp
+++ main.cpp
@@ -1,58 +1,57 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+
 using namespace std;
-class ComplexNumber {
-private:
-    float real;
-    float imaginary;
 
+const int MAX_STUDENTS = 100;
+
+class Student {
 public:
-    ComplexNumber() : real(0.0f), imaginary(0.0f) {}
-    ComplexNumber(float realPart, float imaginaryPart) : real(realPart), imaginary(imaginaryPart) {}
+    string surname;
+    string name;
+    int math;
+    int physics;
+    int computer_science;
 
-    float getReal() const {
-        return real;
-    }
-
-    void setReal(float realPart) {
-        real = realPart;
-    }
-
-    float getImaginary() const {
-        return imaginary;
-    }
-
-    void setImaginary(float imaginaryPart) {
-        imaginary = imaginaryPart;
-    }
-
-    void print() const {
-        cout << "(" << real << "+i" << imaginary << ")";
+    float average_mark() const {
+        return (math + physics + computer_science) / 3.0;
     }
 };
 
-ComplexNumber frd(const ComplexNumber& num1, const ComplexNumber& num2) {
-    float realPart = num1.getReal() + num2.getReal();
-    float imaginaryPart = num1.getImaginary() + num2.getImaginary();
-    return ComplexNumber(realPart, imaginaryPart);
+void bubble_sort(Student arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j].average_mark() < arr[j + 1].average_mark()) {
+                Student temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
 }
 
 
-
 int main() {
-    ComplexNumber complexNum1;
-    ComplexNumber complexNum2(1.5f, 2.5f);
+    Student students[MAX_STUDENTS];
+    int n = 0;
+    ifstream file("data.txt");
+    if (file.is_open()) {
+        while (file >> students[n].surname >> students[n].name >> students[n].math >> students[n].physics >> students[n].computer_science) {
+            n++;
+        }
+        file.close();
+    } else {
+        cout << "Error opening file!" << endl;
+        return 1;
+    }
 
-    cout << "Complex Number 1: ";
-    complexNum1.print();
-    cout << endl;
-
-    cout << "Complex Number 2: ";
-    complexNum2.print();
-    cout << endl;
-
-    ComplexNumber sum = frd(complexNum1, complexNum2);
-    cout << "Sum: ";
-    sum.print();
+    bubble_sort(students, n);
+    for (int i = 0; i < n; i++) {
+        if (students[i].average_mark() > 4.0) {
+            cout << students[i].surname << " " << students[i].name << ": " << students[i].average_mark() << endl;
+        }
+    }
 
     return 0;
 }
